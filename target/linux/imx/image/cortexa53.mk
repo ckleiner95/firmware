dd886fe4880914dd705ba54fd1f5ca3c0f3db170
Corey Wagehoft <corey.wagehoft@gmail.com>
2026-01-21 19:33:27 -0700

    remove factory image gen

--- target/linux/imx/image/cortexa53.mk
+++ target/linux/imx/image/cortexa53.mk
@@ -92,7 +92,7 @@ define Device/gw_venice
   IMAGE_PREFIX = $$(VERSION_DIST_SANITIZED)-$$(IMG_PREFIX_VERCODE)$$(IMG_PREFIX_EXTRA)$$(call sanitize,$$(DEVICE_MODEL)-$$(DEVICE_VARIANT))
   DEVICE_IMG_NAME = $$(IMAGE_PREFIX)-$$(1)-$$(2)
   DEVICE_PACKAGES += kmod-morse netifd-morse morse-fw-8108
-  IMAGES := factory.img.gz sysupgrade.img.gz
+  IMAGES := sysupgrade.img.gz
   IMAGE/sysupgrade.img.gz := boot-scr | boot-img-ext4 | sdcard-img-ext4 | gzip | append-metadata
   IMAGE/factory.img.gz := boot-scr | boot-img-ext4 | sdcard-img-ext4 | gzip
 endef
